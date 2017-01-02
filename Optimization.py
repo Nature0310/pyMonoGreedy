36845dbec7c92c4972454d47defa986197cc47a7
jinjunqi <jinjunqi@163.com>
2017-01-02 21:58:20 +0800

    opt

--- Optimization.py
+++ Optimization.py
@@ -1,4 +1,5 @@
 import logging
+import copy
 
 logging.basicConfig(level=logging.DEBUG)
 logging.info('So should this')
@@ -6,11 +7,22 @@ logging.info('So should this')
 
 class Optimization:
 
-    def __init__(self):
-        self.phi_set
+    def __init__(self, full_set, budget):
+        self.full_set = full_set  # from phi_max
+        self.growing_set = []
+        self.rest_set = copy.deepcopy(full_set)
+        self.budget = budget
 
     def run(self):
+        
+
+        # [p, t] = get_set_p_and_t(set)
+        # deltaX = get_deltaX_minus()  get_deltaX_divide()
+        # output: Su or Ss
 
 
+
+
+if __name__ == '__main__':
 
 
